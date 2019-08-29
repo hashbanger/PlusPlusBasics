f5d5dca39f70e78429f9c1cacb92e6d245dc0afa
hashbanger <prashant.brahmbhatt32@outlook.com>
2019-08-29 19:16:57 +0530

    added namespaces1

---
+++ 02_namespaces_1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+// This is a single line comment
+
+/*
+This is 
+a freakin 
+multiline comment
+*/
+
+int main(){
+
+std::cout<<"Hola de PlusPlus!!!";  // using cout from std namespace
+
+return 0;
+}
