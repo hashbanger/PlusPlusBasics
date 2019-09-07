c1ac757f48cc7e3ab27322b167952b06d59c0427
hashbanger <prashant.brahmbhatt32@outlook.com>
2019-09-07 18:49:21 +0530

    added initialization

---
+++ 06_initialization.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+// int a = 0;  C like initialization 
+//int a (0);   Constructor like initialization
+// int a {0};  C++11 like initialization
+
+
+cout<<"Enter length of room"<<endl;
+int roomLength{0};
+cin>>roomLength;
+
+cout<<"Enter width of the room"<<endl;
+int roomWidth{0};
+cin>>roomWidth;
+
+cout<<"The area of the room is: "<<roomWidth * roomLength<<endl;
+
+return 0;
+}
